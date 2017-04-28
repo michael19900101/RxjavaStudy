88d3422d6a424b11b6c37a07b493670a7e9ad636
huangkunting <382968311@qq.com>
2017-04-28 17:24:50 +0800

    新建RxJava练习项目,写了第一个例子Hello world! 实现被观察订阅观察者的三种方式: 1.Observable.subscribe(Subscriber); 2.Observable.subscribe(Observer); 3.Observable.subscribe(Action); //subscribe() 支持不完整定义的回调,自动创建 ActionSubscriber(继承Subscriber)

---
+++ src/com/aotuman/rxjava/RxJava01.java
@@ -0,0 +1,98 @@
+package com.aotuman.rxjava;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+
+public class RxJava01 {
+	// 被观察者
+	static Observable<String> mObservable = Observable.create(  
+		    new Observable.OnSubscribe<String>() {  
+		    	@Override
+		        public void call(Subscriber<? super String> sub) {  
+		        	System.out.println("被观察者[mObservable]:Hello, world!");
+		            sub.onNext("Hello, world!");  
+		            sub.onCompleted();  
+		        }  
+		    }  
+		); 
+	
+	// 观察者
+	static Subscriber<String> mSubscriber = new Subscriber<String>() {  
+		@Override
+	    public void onNext(String s) { 
+			System.out.println("观察者[mSubscriber]:"+s); 
+		}  
+
+		@Override
+	    public void onCompleted() { }  
+
+		@Override
+	    public void onError(Throwable e) { }  
+	}; 
+	
+	// 观察者
+	static Observer<String> mObserver = new Observer<String>() {
+		@Override
+	    public void onNext(String s) { 
+			System.out.println("观察者[mObserver]:"+s); 
+		}  
+		
+		@Override
+		public void onCompleted() {}
+
+		@Override
+		public void onError(Throwable arg0) {}
+		
+	};
+	
+	static Action1<String> onNextAction = new Action1<String>(){
+		// onNext()
+		@Override
+		public void call(String s) { 
+			System.out.println("onNextAction:"+s); 
+		}	
+	};
+	
+	static Action1<Throwable> onErrorAction = new Action1<Throwable>() {
+	    // onError()
+	    @Override
+	    public void call(Throwable throwable) {
+	    	System.out.println("onError()"); 
+	    }
+	};
+	
+	static Action0 onCompletedAction = new Action0() {
+	    // onCompleted()
+	    @Override
+	    public void call() {
+	    	System.out.println("onCompleted()");
+	    }
+	};
+	
+	public static void test1(){
+		// 被观察者订阅观察者
+		mObservable.subscribe(mSubscriber);  
+	}
+	
+	public static void test2(){
+		// 被观察者订阅观察者
+		mObservable.subscribe(mObserver); 
+	}
+	
+	public static void test3(){
+		// subscribe() 支持不完整定义的回调
+		// 自动创建 ActionSubscriber(继承)，并使用 onNextAction、 onErrorAction 和 onCompletedAction 
+		// 分别定义 onNext()、 onError() 和 onComplete
+		mObservable.subscribe(onNextAction);
+		mObservable.subscribe(onNextAction, onErrorAction);
+		mObservable.subscribe(onNextAction, onErrorAction, onCompletedAction);
+	}
+	
+	public static void main(String[] args) {
+		test1();
+	}
+
+}
